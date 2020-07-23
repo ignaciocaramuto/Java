01ed5a9430d50810a9524df395ae1a8eb7db751e
ignaciocaramuto <63249123+ignaciocaramuto@users.noreply.github.com>
2020-07-23 20:12:45 -0300

    Add files via upload

---
+++ src/entities/Cliente.java
@@ -0,0 +1,102 @@
+package entities;
+
+import java.util.Date;
+
+public abstract class Cliente {
+	private int idCliente;
+	private String tipoDoc;
+	private String numDoc;
+	private String apellido;
+	private String nombre;
+	private String sexo;
+	private Date fechaNacimiento;
+	private String mail;
+	private int telefono;
+	private String usuario;
+	private String contraseña;
+	private String tipoTarjetaCredito;
+	private int numTarjetaCredito;
+	
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+	public void setTipoDoc(String tipoDoc) {
+		this.tipoDoc = tipoDoc;
+	}
+	public String getNumDoc() {
+		return numDoc;
+	}
+	public void setNumDoc(String numDoc) {
+		this.numDoc = numDoc;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public String getTipoTarjetaCredito() {
+		return tipoTarjetaCredito;
+	}
+	public void setTipoTarjetaCredito(String tipoTarjetaCredito) {
+		this.tipoTarjetaCredito = tipoTarjetaCredito;
+	}
+	public int getNumTarjetaCredito() {
+		return numTarjetaCredito;
+	}
+	public void setNumTarjetaCredito(int numTarjetaCredito) {
+		this.numTarjetaCredito = numTarjetaCredito;
+	}
+	
+	
+
+
+}
---
+++ src/entities/Habitacion.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Habitacion {
+	private int nro_Habitacion;
+	private String denominacion;
+	
+	public int getNro_Habitacion() {
+		return nro_Habitacion;
+	}
+	public void setNro_Habitacion(int nro_Habitacion) {
+		this.nro_Habitacion = nro_Habitacion;
+	}
+	public String getDenominacion() {
+		return denominacion;
+	}
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+	
+	
+}
